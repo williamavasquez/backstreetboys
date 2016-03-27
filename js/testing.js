dc60eea6f77ceb834b714ea3bd771b7a819547c0
williamavasquez <william.a.vasquez@gmail.com>
2016-03-26 20:31:37 -0400

    updated testing file

--- js/testing.js
+++ js/testing.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
 	recipes = [];
 
-   var url = 'https://api.edamam.com/search?q=chicken&app_id=82e33b19&app_key=ef430f166ba206b72620a9732916c7e6&to=9';
+   var url = 'https://api.edamam.com/search?q=fish&app_id=82e33b19&app_key=ef430f166ba206b72620a9732916c7e6&to=9';
 
    $.ajax({
       type: 'GET',
@@ -15,9 +15,21 @@ $(document).ready(function(){
 	  }).done(function(response){
   	   	console.log(response);
 		   	test = response;
+        RecipeOne= {
+           image: test.hits[randomizedR].recipe.image,
+           // label: test.hits[randomizedR].recipe.label,
+           // source: test.hits[randomizedR].recipe.source,
+           // shareAs: test.hits[randomizedR].recipe.shareAs,
+         };
+        console.log(RecipeOne.image);
 		   	randomRecipe = test.hits[randomizedR].recipe.ingredients[2].quantity;
 		  	console.log(randomRecipe);
+        EXPR = "<img class='recipeSeven' src="+"'"+RecipeOne.image+"'>"; 
+        console.log(EXPR);
+        $('#randomizedRecipes').prepend(EXPR);
+
+
+
 		   });
- 
 
 });
