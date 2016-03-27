9e8c3341cf121ef97a65d47c417ef0eea7474635
williamavasquez <william.a.vasquez@gmail.com>
2016-03-26 23:44:45 -0400

    changed key and id of the api

--- js/testing.js
+++ js/testing.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   randomizedR = Math.floor(Math.random()*9);
 	recipesShown = [];
 
-   var url = 'https://api.edamam.com/search?q=beef&app_id=82e33b19&app_key=ef430f166ba206b72620a9732916c7e6&to=30';
+   var url = 'https://api.edamam.com/search?q=beef&app_id=e370fe5f&app_key=5515518d09b2185298c869b4fd12db21&to=30';
 
    // $.ajax({
    //    type: 'GET',
