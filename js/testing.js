e7ff804d28120efba7190253ace2acc6f806b1a4
williamavasquez <william.a.vasquez@gmail.com>
2016-03-26 22:58:23 -0400

    random 1 ingr. display

--- js/testing.js
+++ js/testing.js
@@ -1,35 +1,49 @@
 $(document).ready(function(){
   randomizedR = Math.floor(Math.random()*9);
-  console.log(randomizedR);
+	recipesShown = [];
 
-	recipes = [];
+   var url = 'https://api.edamam.com/search?q=beef&app_id=82e33b19&app_key=ef430f166ba206b72620a9732916c7e6&to=30';
 
-   var url = 'https://api.edamam.com/search?q=fish&app_id=82e33b19&app_key=ef430f166ba206b72620a9732916c7e6&to=9';
+   // $.ajax({
+   //    type: 'GET',
+   //     url: url,
+   //     async: false,
+   //     contentType: "application/json",
+   //     dataType: 'jsonp',
+	  // }).done(function(response){
+  	//    	console.log(response);
+		 //   	test = response;
+   //      RecipeOne= {
+   //         image: test.hits[randomizedR].recipe.image,
+   //         // label: test.hits[randomizedR].recipe.label,
+   //         // source: test.hits[randomizedR].recipe.source,
+   //         // shareAs: test.hits[randomizedR].recipe.shareAs,
+   //       };
+   //      console.log(RecipeOne.image);
+   //      // get the ingredient object of a recipe
+   //      // test.hits[randomizedR].recipe.ingredients[X].quantity
 
-   $.ajax({
-      type: 'GET',
-       url: url,
-       async: false,
-       contentType: "application/json",
-       dataType: 'jsonp',
-	  }).done(function(response){
-  	   	console.log(response);
-		   	test = response;
-        RecipeOne= {
-           image: test.hits[randomizedR].recipe.image,
-           // label: test.hits[randomizedR].recipe.label,
-           // source: test.hits[randomizedR].recipe.source,
-           // shareAs: test.hits[randomizedR].recipe.shareAs,
-         };
+for (var i = 7 - 1; i >= 0; i--) {
+  var test = {hits:["",""]};
+
+     $.ajax({
+        type: 'GET',
+        url: url,
+        async: false,
+        contentType: "application/json",
+        dataType: 'jsonp',
+      success: function(results) {
+      genRandomNumber = Math.floor(Math.random()*30);
+        test = results;
+        RecipeOne= test.hits[genRandomNumber].recipe;
+
+        recipesShown.push(test.hits[genRandomNumber]);
         console.log(RecipeOne.image);
-		   	randomRecipe = test.hits[randomizedR].recipe.ingredients[2].quantity;
-		  	console.log(randomRecipe);
         EXPR = "<img class='recipeSeven' src="+"'"+RecipeOne.image+"'>"; 
-        console.log(EXPR);
+
         $('#randomizedRecipes').prepend(EXPR);
-
-
-
-		   });
-
+console.log(recipesShown);
+      },
+  });
+};
 });
