c51fb578dea5bf9d8195e158556dfd6de8929f53
williamavasquez <william.a.vasquez@gmail.com>
2016-03-28 20:34:57 -0400

    recipe titles added

--- js/testing.js
+++ js/testing.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
 	recipesShown = [];
+
   veggies = ["Alfalfa sprouts","Artichoke","Arugula","Asparagus","Basil","Bean sprouts","bell pepper","Black beans","Black-eyed peas","Broad beans","Broccoli","Brussels sprouts","Butternut squash","Cabbage","Carrot","Carrots","Cauliflower","Celery","Chives","Collard greens","Corn","Daikon","Eggplant","Fennel","Frisee","garbanzo","Garlic","Ginger","Green beans","Habanero","Horseradish","Jalapeño","Kale","Kidney beans","Legumes","Lemon Grass","Lentils","lentils","Lettuce","lima beans","Marjoram","mushroom","Mustard greens","Nettles","spinach","Okra","Onion","Oregano","Paprika","Parsley","Parsnip","Peas","peppers","Pinto beans","Potato","Pumpkin","Radish","Shallot","Skirret","Soy beans","Spaghetti squash","Spinach","scallion","Sweet potato","Tabasco pepper","Tomato","Water chestnut","Watercress","yam","Zucchini"];
+
   meats=["beef","chicken","pork","salmon","tilapia","lamb", "turkey"];
   genRandomNumber = Math.floor(Math.random()*meats.length);
-console.log(meats[genRandomNumber]);
-console.log(veggies[genRandomNumber]);
-
-rMeat=meats[genRandomNumber]
-rVeggie = veggies[Math.floor(Math.random()*veggies.length)]
+  rMeat=meats[genRandomNumber];
+  rVeggie = veggies[Math.floor(Math.random()*veggies.length)];
 
    var url = 'https://api.edamam.com/search?q='+rMeat+','+rVeggie+'&app_id=e370fe5f&app_key=5515518d09b2185298c869b4fd12db21&to=30';
 
@@ -36,8 +35,14 @@ rVeggie = veggies[Math.floor(Math.random()*veggies.length)]
         recipesShown.push(test.hits[genRandomNumber]);
         console.log(RecipeOne.image);
         EXPR = "<img class='recipeSeven' src="+"'"+RecipeOne.image+"'>"; 
+        RecipeName = RecipeOne.label;
+       
+        Rdiv = $('<div>').addClass("recipeBoxes");
+        RecipeName = $('<h2>').text(RecipeName);
+        Rdiv= Rdiv.append(RecipeName)
+        Rdiv= Rdiv.append(EXPR);
 
-        $('#image').prepend(EXPR);
+        $('#image').prepend(Rdiv);
         console.log(recipesShown);
       };
       }
