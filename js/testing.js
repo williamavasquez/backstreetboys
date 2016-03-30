d6f9dcc645202617dbf50a76e01bdfc90cb7f06f
williamavasquez <william.a.vasquez@gmail.com>
2016-03-29 23:42:35 -0400

    working random recipe function

--- js/testing.js
+++ js/testing.js
@@ -1,48 +1,43 @@
 $(document).ready(function(){
-	recipesShown = [];
 
-   var url = 'https://api.edamam.com/search?q=beef&app_id=e370fe5f&app_key=5515518d09b2185298c869b4fd12db21&to=30';
+  veggies = ["Alfalfa sprouts","Artichoke","Arugula","Asparagus","Basil","Bean sprouts","bell pepper","Black beans","Black-eyed peas","Broad beans","Broccoli","Brussels sprouts","Butternut squash","Cabbage","Carrot","Carrots","Cauliflower","Celery","Chives","Collard greens","Corn","Daikon","Eggplant","garbanzo","Garlic","Ginger","Green beans","Habanero","Horseradish","Jalapeño","Kale","Kidney beans","Legumes","Lemon Grass","Lentils","lentils","Lettuce","lima beans","Marjoram","mushroom","Mustard greens","Nettles","spinach","Okra","Onion","Oregano","Paprika","Parsley","Parsnip","Peas","peppers","Pinto beans","Potato","Pumpkin","Radish","Shallot","Skirret","Soy beans","Spaghetti squash","Spinach","scallion","Sweet potato","Tabasco pepper","Tomato","Water chestnut","Watercress","yam","Zucchini"];
 
-  veggies = ["Alfalfa sprouts","Artichoke","Arugula","Asparagus","Basil","Bean sprouts","bell pepper","Black beans","Black-eyed peas","Broad beans","Broccoli","Brussels sprouts","Butternut squash","Cabbage","Carrot","Carrots","Cauliflower","Celery","Chives","Collard greens","Corn","Daikon","Eggplant","Fennel","Frisee","garbanzo","Garlic","Ginger","Green beans","Habanero","Horseradish","Jalapeño","Kale","Kidney beans","Legumes","Lemon Grass","Lentils","lentils","Lettuce","lima beans","Marjoram","mushroom","Mustard greens","Nettles","spinach","Okra","Onion","Oregano","Paprika","Parsley","Parsnip","Peas","peppers","Pinto beans","Potato","Pumpkin","Radish","Shallot","Skirret","Soy beans","Spaghetti squash","Spinach","scallion","Sweet potato","Tabasco pepper","Tomato","Water chestnut","Watercress","yam","Zucchini"];
+  meats=["beef","chicken","pork","salmon","tilapia","lamb", "turkey","tofu","fish","duck", "steak"];
 
-  meats=["beef","chicken","pork","salmon","tilapia","lamb", "turkey"];
-  genRandomNumber = Math.floor(Math.random()*meats.length);
-  rMeat=meats[genRandomNumber];
-  rVeggie = veggies[Math.floor(Math.random()*veggies.length)];
+recipesShown = [];
 
-   var url = 'https://api.edamam.com/search?q='+rMeat+','+rVeggie+'&app_id=e370fe5f&app_key=5515518d09b2185298c869b4fd12db21&to=30';
+     for (var i = 6; i >= 0; i--) {
+      randomRecipe();
+      }
 
-   //      console.log(RecipeOne.image);
-   //      // get the ingredient object of a recipe
-   //      // test.hits[randomizedR].recipe.ingredients[X].quantity
+  function randomRecipe(){
+    SingleRecipe=[];
+    Rmeat = Math.floor(Math.random()*meats.length);
+    Rveggies = Math.floor(Math.random()*veggies.length);
+    RRecipe = Math.floor(Math.random()*10)
 
-     $.ajax({
+    var url = 'https://api.edamam.com/search?q='+meats[Rmeat]+','+veggies[Rveggies]+'&from='+RRecipe+'&to='+(RRecipe+1)+'&app_id=e370fe5f&app_key=5515518d09b2185298c869b4fd12db21';
+
+    $.ajax({
         type: 'GET',
         url: url,
         async: true,
         contentType: "application/json",
         dataType: 'jsonp',
-      success: function(results) {
-        console.log(results);
-      for (var i = 6; i >= 0; i--) {
-        test = results;
-        genRandomNumber = Math.floor(Math.random()*results.hits.length);
-        RecipeOne= test.hits[genRandomNumber].recipe;
-        
-        recipesShown.push(test.hits[genRandomNumber]);
-        console.log(RecipeOne.image);
-        EXPR = "<img class='recipeSeven' src="+"'"+RecipeOne.image+"'>"; 
-        RecipeName = RecipeOne.label;
-       
-        Rdiv = $('<div>').addClass("recipeBoxes");
-        RecipeName = $('<h2>').text(RecipeName);
-        Rdiv= Rdiv.append(RecipeName)
-        Rdiv= Rdiv.append(EXPR);
+        success: function(results) {
+          test = results;
+          console.log(test);
+          RecipeOne= test.hits[0].recipe;
+          recipesShown.push(RecipeOne);
+          SingleRecipe.push(RecipeOne);
 
-        $('#image').prepend(Rdiv);
-        console.log(recipesShown);
-      };
-      }
-  });
-
+    EXPR = "<img class='recipeSeven' src="+"'"+RecipeOne.image+"'>"; 
+    Rdiv = $('<div>').addClass("recipeBoxes");
+    RecipeName = $('<h2>').text(RecipeOne.label);
+    Rdiv= Rdiv.append(RecipeName)
+    Rdiv= Rdiv.append(EXPR);
+    $('#image').prepend(Rdiv);
+        }
+      });
+  }
 });
