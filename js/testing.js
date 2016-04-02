f795f0365a9bd4537dc5cba95cc5ec2fedfa52df
williamavasquez <william.a.vasquez@gmail.com>
2016-04-02 08:13:51 -0400

    spinner gone - no more

--- js/testing.js
+++ js/testing.js
@@ -34,7 +34,6 @@ function checkRecipes(){
       }
     } else {
         recipesShown.splice(7,100);
-        spinner.stop();
         for (var i = 0; i < 7; i++) {
           RecipeOne=recipesShown[i];
           EXPR = "<img class='recipeSeven' src="+"'"+RecipeOne.image+"'>"; 
@@ -140,31 +139,7 @@ dontHaveItems=[];
 //    }).spin(document.getElementById("#myProgress"));
 // })
 // spinner.js spin the spinning spinner spun span spong //
-console.log("spin span spun");
-var opts = {
-  lines: 13 // The number of lines to draw
-, length: 28 // The length of each line
-, width: 14 // The line thickness
-, radius: 42 // The radius of the inner circle
-, scale: 1 // Scales overall size of the spinner
-, corners: 1 // Corner roundness (0..1)
-, color: '#000' // #rgb or #rrggbb or array of colors
-, opacity: 0.25 // Opacity of the lines
-, rotate: 0 // The rotation offset
-, direction: 1 // 1: clockwise, -1: counterclockwise
-, speed: 1 // Rounds per second
-, trail: 60 // Afterglow percentage
-, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-, zIndex: 2e9 // The z-index (defaults to 2000000000)
-, className: 'spinner' // The CSS class to assign to the spinner
-, top: '55%' // Top position relative to parent
-, left: '50%' // Left position relative to parent
-, shadow: false // Whether to render a shadow
-, hwaccel: false // Whether to use hardware acceleration
-, position: 'absolute' // Element positioning
-}
-var target = document.getElementById('myProgress');
-var spinner = new Spinner(opts).spin(target);
+
 $('#submitWunderlist').on('click', function(){
   $('#ingredient').hide();
   $('#enjoy').show();
